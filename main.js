611a8441a4deaa9686926dc40c72c0b82e5b8a80
NxReason <arsiddde@gmail.com>
2025-02-10 16:16:36 +0300

    initial

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron/main');
+const path = require('node:path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+  win.webContents.openDevTools();
+  // win.loadFile('index.html'); // for prod
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
